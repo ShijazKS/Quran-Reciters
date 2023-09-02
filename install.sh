bd48163a41c0b3d7b986652cef0c5a9314b3d5e1
ShijazKs <hafizshijaz@gmail.com>
2023-09-02 13:35:22 +0000

    install.sh update

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/ShijazKS/Quran-Reciters.git
+
+# Move into the repository directory
+cd Quran-Reciters
+
+# Make the Python script executable
+chmod +x quran_recitation.py
+
+# Optionally, move the script to a location in the user's PATH
+mv quran_recitation.py ~/bin/Quran
+
+echo "Installation complete."
+echo "You can now run the program by typing 'Quran' in the terminal."
